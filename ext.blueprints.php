59be98c5642e9c9239c7e1132d9ce16dc675d95e
Brian Litzinger <litzinger@gmail.com>
2011-09-29 11:10:35 -0500

    Updating to more detailed licensing

--- ext.blueprints.php
+++ ext.blueprints.php
@@ -17,8 +17,33 @@ if (! defined('BLUEPRINTS_VERSION'))
  * @subpackage  Extensions
  * @category    Blueprints
  * @author      Brian Litzinger
- * @copyright   Copyright 2011 to infinity and beyond! - Boldminded / Brian Litzinger
+ * @copyright   Copyright (c) 2010, 2011 - Brian Litzinger
  * @link        http://boldminded.com/add-ons/blueprints
+ * @license 
+ *
+ * Copyright (c) 2011, 2012. BoldMinded, LLC
+ * All rights reserved.
+ *
+ * This source is commercial software. Use of this software requires a
+ * site license for each domain it is used on. Use of this software or any
+ * of its source code without express written permission in the form of
+ * a purchased commercial or other license is prohibited.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * As part of the license agreement for this software, all modifications
+ * to this source must be submitted to the original author for review and
+ * possible inclusion in future releases. No compensation will be provided
+ * for patches, although where possible we will attribute each contribution
+ * in file revision notes. Submitting such modifications constitutes
+ * assignment of copyright to the original author (Brian Litzinger and
+ * BoldMinded, LLC) for such modifications. If you do not wish to assign
+ * copyright to the original author, your license to  use and modify this
+ * source is null and void. Use of this software constitutes your agreement
+ * to this clause.
  */
  
 class Blueprints_ext {
