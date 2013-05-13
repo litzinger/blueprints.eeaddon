1e2e6865dddf97d2b0349c74f2d7af35e379c46e
Brian Litzinger <litzinger@gmail.com>
2013-05-12 19:48:38 -0500

    version bump

--- third_party/blueprints/config.php
+++ third_party/blueprints/config.php
@@ -1,6 +1,6 @@
 <?php
 $config['name'] = 'Blueprints';
-$config['version'] = '2.0.5';
+$config['version'] = '2.0.6';
 $config['description'] = 'Assign Publish Layouts to Structure templates.';
 $config['docs_url'] = 'http://boldminded.com/add-ons/blueprints';
 $config['nsm_addon_updater']['versions_xml'] = 'http://boldminded.com/versions/blueprints';
