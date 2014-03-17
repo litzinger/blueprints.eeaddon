1c8eda974d1948773e05302ceb9b656429f44d5b
Brian Litzinger <blitzing@nerdery.com>
2014-03-17 16:17:14 -0500

    Fixed issue with EE 2.8 where it was not reporting as being in the publish page.

--- third_party/blueprints/config.php
+++ third_party/blueprints/config.php
@@ -1,6 +1,6 @@
 <?php
 $config['name'] = 'Blueprints';
-$config['version'] = '2.0.9';
+$config['version'] = '2.0.10';
 $config['description'] = 'Assign Publish Layouts to Structure templates.';
 $config['docs_url'] = 'http://boldminded.com/add-ons/blueprints';
 $config['nsm_addon_updater']['versions_xml'] = 'http://boldminded.com/versions/blueprints';
--- third_party/blueprints/helper.blueprints.php
+++ third_party/blueprints/helper.blueprints.php
@@ -9,7 +9,7 @@
  * @author      Brian Litzinger
  * @copyright   Copyright (c) 2010, 2011 - Brian Litzinger
  * @link        http://boldminded.com/add-ons/blueprints
- * @license 
+ * @license
  *
  * Copyright (c) 2011, 2012. BoldMinded, LLC
  * All rights reserved.
@@ -40,15 +40,15 @@ class Blueprints_helper
 {
     private $site_id;
     private $cache;
-    
+
     public $thumbnail_directory_url;
     public $thumbnail_directory_path;
-    
+
     public function __construct()
     {
         $this->EE =& get_instance();
         $this->site_id = $this->EE->config->item('site_id');
-        
+
         // Create cache
         if (! isset($this->EE->session->cache['blueprints']))
         {
@@ -56,56 +56,56 @@ class Blueprints_helper
         }
         $this->cache =& $this->EE->session->cache['blueprints'];
     }
-    
+
     public function get_checkbox_options($k)
     {
         $templates = $this->EE->blueprints_model->get_templates();
-        
+
         $checkbox_options = '';
         $groups = array();
-        
+
         foreach($templates->result_array() as $template)
         {
             if(!in_array($template['group_name'], $groups))
             {
                 $checked = ((
-                        isset($template['group_name']) AND 
+                        isset($template['group_name']) AND
                         isset($this->cache['settings']['channel_show_group']) AND
-                        isset($this->cache['settings']['channel_show_group'][$k]) AND 
+                        isset($this->cache['settings']['channel_show_group'][$k]) AND
                         in_array($template['group_name'], $this->cache['settings']['channel_show_group'][$k])
                 )) ? TRUE : FALSE;
-                
+
                 $checkbox_options .= '<p>';
                 $checkbox_options .= form_checkbox(
-                                        'channel_show_group['. $k .'][]', 
-                                        $template['group_name'], 
-                                        $checked, 
+                                        'channel_show_group['. $k .'][]',
+                                        $template['group_name'],
+                                        $checked,
                                         'class="show_group" id="channel_show_group['. $k .']['. $template['group_name'] .']"'
                                     );
-                
+
                 $checkbox_options .= ' <label for="channel_show_group['. $k .']['. $template['group_name'] .']">Show all <i>'. $template['group_name'] .'</i> templates</label>';
             }
             $groups[] = $template['group_name'];
         }
-        
+
         $checked = (
             isset($this->cache['settings']['channel_show_selected']) AND
-            isset($this->cache['settings']['channel_show_selected'][$k]) AND 
+            isset($this->cache['settings']['channel_show_selected'][$k]) AND
             $this->cache['settings']['channel_show_selected'][$k] == 'y'
         ) ? TRUE : FALSE;
-        
+
         $checkbox_options .= '<p>'. form_checkbox(
-                                        'channel_show_selected['. $k .']', 
+                                        'channel_show_selected['. $k .']',
                                         'y',
                                         $checked,
                                         'id="channel_show_selected['. $k .']" class="show_selected"'
                                     );
-                                    
+
         $checkbox_options .= ' <label for="channel_show_selected['. $k .']">Show only specific templates</label></p>';
-        
+
         return $checkbox_options;
     }
-    
+
     public function get_pages()
     {
         // Make sure pages cache is empty, and also see if we are in the CP. Since fieldtype files get loaded
@@ -113,7 +113,7 @@ class Blueprints_helper
         if(!isset($this->cache['pages']))
         {
             $this->cache['pages'] = "";
-            
+
             if(array_key_exists('structure', $this->EE->addons->get_installed()))
             {
                 require_once $this->get_theme_folder_path().'boldminded_themes/libraries/structure_pages.php';
@@ -130,24 +130,24 @@ class Blueprints_helper
 
         return $this->cache['pages'];
     }
-    
+
     public function get_theme_folder_path()
     {
         return PATH_THEMES . 'third_party/';
     }
-    
+
     public function get_theme_folder_url()
     {
         return $this->EE->config->slash_item('theme_folder_url') .'third_party/';
     }
-    
+
     public function enable_publish_layout_takeover()
     {
         if(!isset($this->cache['enable_publish_layout_takeover']))
         {
             $this->cache['enable_publish_layout_takeover'] = (isset($this->cache['settings']['enable_publish_layout_takeover']) AND $this->cache['settings']['enable_publish_layout_takeover'] == 'y') ? true : false;
         }
-        
+
         return $this->cache['enable_publish_layout_takeover'];
     }
 
@@ -182,14 +182,14 @@ class Blueprints_helper
 
         return array_values($new_array);
     }
-    
+
     public function get_site_index()
     {
         $site_index = $this->EE->config->item('site_index');
         $index_page = $this->EE->config->item('index_page');
-        
+
         $index = ($site_index != '') ? $site_index : (($index_page != '') ? $index_page : 'index.php');
-        
+
         return reduce_double_slashes($this->EE->config->slash_item('site_url') . $index);
     }
 
@@ -203,7 +203,7 @@ class Blueprints_helper
                 $this->cache['upload_prefs'] = $this->EE->file_upload_preferences_model->get_file_upload_preferences($group_id, $id);
                 return $this->cache['upload_prefs'];
             }
-            
+
             if (version_compare(APP_VER, '2.1.5', '>='))
             {
                 $this->EE->load->model('file_upload_preferences_model');
@@ -266,29 +266,38 @@ class Blueprints_helper
 
         return str_replace($tokens, $urls, $str);
     }
-    
+
     public function is_publish_form()
     {
         if(REQ != "CP")
         {
             return false;
         }
-        
-        // if($this->EE->router->class == 'content_publish' AND $this->EE->router->method == 'entry_form')
-        // if(
-        //     ($this->EE->input->get('C') == 'content_publish' AND $this->EE->input->get('M') == 'entry_form') OR
-        //     ($this->EE->input->get('C') == 'javascript' AND $this->EE->input->get('M') == 'load')
-        // ){
-        if($this->EE->input->get('C') == 'content_publish' AND $this->EE->input->get('M') == 'entry_form')
+
+        if (version_compare(APP_VER, '2.8', '>='))
         {
-            return true;
+            if($this->EE->router->class == 'content_publish' AND $this->EE->router->method == 'entry_form')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
-            return false;
+            if($this->EE->input->get('C') == 'content_publish' AND $this->EE->input->get('M') == 'entry_form')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
-    
+
     public function load_switch($target)
     {
         if(!$target)
@@ -300,10 +309,10 @@ class Blueprints_helper
         {
             $this->EE->cp->add_to_head('<link rel="stylesheet" type="text/css" href="'.$this->get_theme_folder_url().'pt_switch/styles/pt_switch.css" />');
             $this->EE->cp->add_to_foot('<script type="text/javascript" src="'.$this->get_theme_folder_url().'pt_switch/scripts/pt_switch.js"></script>');
-            
+
             $this->cache['pt_switch'] = true;
         }
-        
+
         if(is_array($target))
         {
             foreach($target as $ele)
@@ -316,7 +325,7 @@ class Blueprints_helper
             $this->EE->cp->add_to_foot('<script type="text/javascript">new ptSwitch(jQuery("'. $target .'"));</script>');
         }
     }
-    
+
     /**
       * Retrieve site path
       */
@@ -325,14 +334,14 @@ class Blueprints_helper
         $site_url = $this->EE->config->slash_item('site_path');
         return $site_url ? $site_url : str_replace('themes/', '', PATH_THEMES);
     }
-    
+
     private function debug($str, $die = false)
     {
         echo '<pre>';
         var_dump($str);
         echo '</pre>';
-        
+
         if($die) die('debug terminated');
     }
-    
+
 }
