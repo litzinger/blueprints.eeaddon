1bede7be52302a505e400514924df152cfadf2dd
Isaac Raway <isaac@mm.st>
2015-03-03 21:57:30 -0800

    Fix some random undefined errors probably caused by recent EE changes

--- third_party/blueprints/ext.blueprints.php
+++ third_party/blueprints/ext.blueprints.php
@@ -73,7 +73,7 @@ class Blueprints_ext {
     /**
      * Constructor
      */
-    public function Blueprints_ext($settings = '')
+    public function __construct($settings = '')
     {
         if (REQ != 'CP') return;
 
@@ -114,9 +114,6 @@ class Blueprints_ext {
     */
     public function sessions_end($session)
     {
-        // Structure and other third-party things triggered by this may choke if session is not set
-        $this->EE->session = $session;
-
         // Stop here if we shouldn't be hi-jacking the publish layouts
         if(
             REQ != 'CP' OR
@@ -589,7 +586,7 @@ class Blueprints_ext {
     */
     public function channel_entries_tagdata($tagdata, $row, $obj)
     {
-        if(!isset($this->EE)) $this->EE = &get_instance();
+        $this->EE = &get_instance();
         
         if($this->EE->extensions->last_call)
         {
@@ -599,24 +596,24 @@ class Blueprints_ext {
         $entry_id = $row['entry_id'];
 
         // Get templates
-        $site_pages = $this->EE->config->item('site_pages');
-        $templates  = $site_pages[$this->EE->config->item('site_id')]['templates'];
+        $site_pages = ee()->config->item('site_pages');
+        $templates  = $site_pages[ee()->config->item('site_id')]['templates'];
 
         $template_id = isset($templates[$entry_id]) ? $templates[$entry_id] : FALSE;
         $template_path = FALSE;
 
         if ($template_id)
         {
-            $qry = $this->EE->db->get_where('templates', array('template_id' => $template_id));
+            $qry = ee()->db->get_where('templates', array('template_id' => $template_id));
             $template = $qry->row();
 
-            $qry = $this->EE->db->get_where('template_groups', array('group_id' => $template->group_id));
+            $qry = ee()->db->get_where('template_groups', array('group_id' => $template->group_id));
             $group = $qry->row();
 
             $template_path = $group->group_name . '/' . $template->template_name;
         }
 
-        $tagdata = $this->EE->TMPL->parse_variables_row($tagdata, array(
+        $tagdata = ee()->TMPL->parse_variables_row($tagdata, array(
             'pages_template_id' => $template_id,
             'pages_template_path' => $template_path,
         ));
